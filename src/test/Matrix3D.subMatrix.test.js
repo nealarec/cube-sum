39d53879a36bff55cde5e2ca71f5423922587add
Nelson Alberto Arbelaez <nealarec@gmail.com>
2018-03-04 18:10:14 -0500

    se prueba modificar y extraer el resultado de una matriz de 20*20*20

--- src/test/Matrix3D.subMatrix.test.js
+++ src/test/Matrix3D.subMatrix.test.js
@@ -122,3 +122,17 @@ it('Matrix3D.subMatrix(1,1,1,2,2,3) after make(3) and set(1,1,3,5) will be (2*2*
     ]))
 })
 
+it('Matrix3D.subMatrix(10,10,10,12,12,12) after make(20) and set(11,11,11,5) will be (3*3*3) matrix with 0 and 5 in (2,2,2)', function () {
+    let my_matrix = new Matrix3D
+    my_matrix.make(20)
+    my_matrix.set(11,11,11,5)
+
+    let res = my_matrix.subMatrix(10,10,10,12,12,12)
+
+    expect(JSON.stringify(res)).toBe(JSON.stringify([
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        [[0, 0, 0], [0, 5, 0], [0, 0, 0]],
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    ]))
+})
+
