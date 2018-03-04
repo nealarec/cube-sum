4afe8c95bb496d8afd227aea4070400b80c2ee39
Nelson Alberto Arbelaez <nealarec@gmail.com>
2018-03-04 12:43:32 -0500

    Closes #12: Implementacion y pruebas de Matrix3D.make

--- src/app/classes/Matrix3D.js
+++ src/app/classes/Matrix3D.js
@@ -1,3 +1,15 @@
 export default class Matrix3D {
-    
+    matrix = []
+
+    /**
+     * Create a matrix of n*n*n and store it on matrix property
+     * @param {number} n determinate the matrix length
+     */
+    make(n) {
+        this.matrix = [...Array(n).keys()].map(() =>
+            [...Array(n).keys()].map(() =>
+                [...Array(n).keys()].map(() => 0)
+            )
+        )
+    }
 }
