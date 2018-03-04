95dadb30cde34cbe4a4eec766f8c60d9029f8dd0
Nelson Alberto Arbelaez <nealarec@gmail.com>
2018-03-04 11:14:44 -0500

    Closes #8 se crea la clase app/classes/Matrix3D

---
+++ src/test/Matrix3D.test.js
@@ -0,0 +1,6 @@
+import Matrix3D  from '../app/classes/Matrix3D'
+
+it('Matrix3D will instantiable ', function () {
+    let my_matrix = new Matrix3D;
+    expect(my_matrix instanceof Matrix3D).toBe(true);
+})
